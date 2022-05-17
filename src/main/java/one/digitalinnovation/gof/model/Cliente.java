91c5de163139ead47c82afea690f06ae79c26c30
JailsonAlm <jailson.07almeida@gmail.com>
2022-05-16 23:34:18 -0300

    creating entities

---
+++ src/main/java/one/digitalinnovation/gof/model/Cliente.java
@@ -0,0 +1,8 @@
+package one.digitalinnovation.gof.model;
+
+/**
+ * Localhost/8080/swagger-ui.html
+ */
+
+public class Cliente {
+}
---
+++ src/main/java/one/digitalinnovation/gof/model/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package one.digitalinnovation.gof.model;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Interface repository, será a interface que irá prover todos os métodos de acesso a dados da determinada entidade
+ */
+
+@Repository
+public class EnderecoRepository {
+}
